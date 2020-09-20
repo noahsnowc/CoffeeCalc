ce075b4a181805366ca6fcf9d8124331e0045392
Noah Snow C <noahsnowc@gmail.com>
2020-09-20 12:21:37 -0400

    Saved user content to file

--- CoffeeCalc.py
+++ CoffeeCalc.py
@@ -1,12 +1,45 @@
 import sys
 import PySimpleGUI as pygui
 import time
+import json
 
 def ratioCalc(groundWeight, chosenRatio):
     if isinstance(groundWeight, int):
         if isinstance(chosenRatio, int):
             return groundWeight * chosenRatio
 
+
+def saveCurrentSelections(weight, ratio):
+    
+    data = {"weight": weight, "ratio": ratio}
+    with open('userSetting.json', 'w') as json_file:
+        json.dump(data, json_file)
+
+def eventLoop(window,timer_running, counter):
+    while True:                             
+        event, values = window.read(timeout=10)
+        if event in (pygui.WIN_CLOSED, 'Quit'):             # if user closed the window using X or clicked Quit button
+
+            break
+
+        elif event == 'Start/Stop':
+            timer_running = not timer_running
+        elif event == 'Reset':
+            counter = 0
+            timer_running = False
+            window['timer'].update('{:02d}:{:02d}.{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
+        if timer_running:
+            window['timer'].update('{:02d}:{:02d}.{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
+            counter += 1
+
+        if event == 'Submit':
+            try:
+                window['BloomOutput'].update(str(ratioCalc(int(values['GramInput']),2))+'g')
+                window['TotalOutput'].update(str(ratioCalc(int(values['GramInput']),int(values['ratioDropdown'])))+'g')
+            except Exception:
+                pygui.popup("Looks like you didn't input a valid number.")
+        saveCurrentSelections(values['GramInput'], values['ratioDropdown'])
+    window.close()
 
 def main():
     pygui.theme('DarkBlue1')
@@ -25,25 +58,6 @@ def main():
 
     timer_running, counter = False, 0
 
-    #event loop
-    while True:                             
-        event, values = window.read(timeout=10) 
-        if event in (pygui.WIN_CLOSED, 'Quit'):             # if user closed the window using X or clicked Quit button
-            break
-        elif event == 'Start/Stop':
-            timer_running = not timer_running
-        elif event == 'Reset':
-            counter = 0
-            timer_running = False
-            window['timer'].update('{:02d}:{:02d}.{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
-        if timer_running:
-            window['timer'].update('{:02d}:{:02d}.{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
-            counter += 1
-        if event == 'Submit':
-            try:
-                window['BloomOutput'].update(str(ratioCalc(int(values['GramInput']),2))+'g')
-                window['TotalOutput'].update(str(ratioCalc(int(values['GramInput']),int(values['ratioDropdown'])))+'g')
-            except Exception:
-                pygui.popup("Looks like you didn't input a valid number.")
-    window.close()
+    eventLoop(window, timer_running, counter)
+    
 main()
